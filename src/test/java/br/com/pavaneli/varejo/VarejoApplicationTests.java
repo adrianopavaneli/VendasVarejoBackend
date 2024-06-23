154730919375d003e3142d49bd91575dd5d14b82
Adriano Pavaneli <adriano.pavaneli.dev@gmail.com>
2024-06-23 20:37:52 -0300

    Criação do projeto

---
+++ src/test/java/br/com/pavaneli/varejo/VarejoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pavaneli.varejo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VarejoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
